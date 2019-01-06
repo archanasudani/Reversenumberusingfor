7d4150b1c150e249cfd977064d78fda0fca28b07
archana sudani <archanasudani@gmail.com>
2019-01-06 08:22:11 +0000

    My First Commit

---
+++ src/loveProgramming/ForLoopReverseNumbers.java
@@ -0,0 +1,12 @@
+package loveProgramming;
+
+public class ForLoopReverseNumbers {
+
+            public static void main(String[] args) {
+            System.out.println("Starting With 50");
+              for (int i = 50; i > 0; i--) {
+//            for (int i = 0; i < 50; i++){
+                System.out.println(i);
+            }
+        }
+}
